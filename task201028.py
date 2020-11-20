c695e8df27d50542a33baacb39df241e333fd096
YasuakinoMBP <cyberdinet800@yahoo.co.jp>
2020-11-20 12:28:13 +0900

    task201028

---
+++ task201028.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python3
+# coding: utf-8
+
+import prc_and, sys
+#print(sys.path)
+
+print("(0,0): ",prc_and.AND(0,0))
+print("(0,1): ",prc_and.AND(0,1))
+print("(1,0): ",prc_and.AND(1,0))
+print("(1,1): ",prc_and.AND(1,1))
+
